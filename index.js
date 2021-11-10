a386729261c26c6e1e46e9053501f851fdb2d2df
Akeem <AkeemS11788@gmail.com>
2021-11-10 00:27:37 -0500

    commit tree 55b47d14ee53e45c571c48aa195631f2cd3bcd48

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+function fetchBooks() { 
+// returns fetch to give test access
+return fetch('https://anapioficeandfire.com/api/books')
+// converts fetch to json for object readability
+.then(response => response.json())
+// invokes renderBooks function passing it as a JSON object
+.then(books => renderBooks(books))
 }
+
+//arrow function way of writing fetch function
+// let fetchBooks = () = fetch('https://anapioficeandfire.com/api/books')
+//.then(response => response.json())
+//.then(books => renderBooks(books))
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
